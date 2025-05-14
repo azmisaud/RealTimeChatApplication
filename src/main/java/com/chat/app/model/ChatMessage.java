56a596ffd77021793b8085da66ba7f9d8032eefd
Mohd Saud <157240150+azmisaud@users.noreply.github.com>
2025-05-14 19:43:20 +0530

    Commit

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,42 @@
+package com.chat.app.model;
+
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+
+    public ChatMessage() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ChatMessage{" +
+                "id=" + id +
+                ", sender='" + sender + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
